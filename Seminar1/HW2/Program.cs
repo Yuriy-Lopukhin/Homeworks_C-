980487bf1f96a8b6a7c31a8b9358282e4c939baa
Yuriy <yuriylopukhin@mail.ru>
2023-02-23 22:54:28 +0300

    Решение задачи №2!

---
+++ Seminar1/HW2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int FirstUserNumber = int.Parse(Console.ReadLine() ?? "");    //Ввод первого числа
+Console.Write("Введите первое число: ");
+int SecondUserNumber = int.Parse(Console.ReadLine() ?? "");    //Ввод первого числа
+
+if (FirstUserNumber > SecondUserNumber) 
+      Console.WriteLine($"a = {FirstUserNumber} b = {SecondUserNumber} -> max = {FirstUserNumber}");  
+else if (FirstUserNumber < SecondUserNumber)
+        Console.WriteLine($"a = {FirstUserNumber} b = {SecondUserNumber} -> max = {SecondUserNumber}");
+else
+        Console.WriteLine($"a = {FirstUserNumber} b = {SecondUserNumber} -> Оба числа равны!");

cf4088ea5f6958cefc709af701dc5c79379dfb99
Yuriy <yuriylopukhin@mail.ru>
2023-02-23 23:56:17 +0300

    Решение задачи №6!

---
+++ Seminar1/HW6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите целое число: ");
+int UserNumber = int.Parse(Console.ReadLine() ?? "");
+
+if (UserNumber % 2 == 0)
+    Console.WriteLine($"{UserNumber} -> Да!");
+else
+    Console.WriteLine($"{UserNumber} -> Нет!");
+

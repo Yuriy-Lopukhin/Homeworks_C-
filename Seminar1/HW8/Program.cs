60a8aeaaa6017664379e6d08daf365bb750f594b
Yuriy <yuriylopukhin@mail.ru>
2023-02-24 10:23:31 +0300

    Решение задачи №8 добавлено!

---
+++ Seminar1/HW8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите целое число: ");
+int UserNumber = int.Parse(Console.ReadLine() ?? "");    //Ввод первого числа
+int i = 1;
+Console.Write($"{UserNumber} -> ");
+while (i < UserNumber)
+{
+    i = i + 1;
+    if (i % 2 == 0)
+    {
+        Console.Write($" {i}, ");
+        i = i + 1;
+    }
+    else
+    {
+        i = i + 1;
+    }
+
+}
+

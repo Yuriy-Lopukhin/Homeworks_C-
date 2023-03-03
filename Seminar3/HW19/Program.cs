d294044a90ab96f83a56ae4707e0e6eec129893e
Yuriy <yuriylopukhin@mail.ru>
2023-03-03 19:42:32 +0300

    Решение задач третьего семинара!

---
+++ Seminar3/HW19/Program.cs
@@ -0,0 +1,15 @@
+﻿int num = new Random().Next(10000, 100000);
+int a1 = num/10000;
+int a2 = (num/1000)%10;
+int a3 = (num/100)%10;
+int a4 = (num/10)%10;
+int a5 = num%10;
+
+if((a1 == a5) && (a2 == a4)) 
+{
+    Console.WriteLine($"Введено число {num}, это палиндром!");
+}
+else
+{
+  Console.WriteLine($"Введено число {num}, это не палиндром!");  
+}
---
+++ Seminar3/HW23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число: ");
+int UserNum = int.Parse(Console.ReadLine() ?? "");
+int i = 1;
+Console.Write($"{UserNum} -> ");
+
+while (i < UserNum) 
+{
+    int Number = i*i*i;
+    Console.Write($"{Number} ");
+    i = i+1;
+}

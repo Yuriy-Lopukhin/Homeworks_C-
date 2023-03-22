9f4d41facbacd5fdfe6c66233aa01f80a6120502
Yuriy <yuriylopukhin@mail.ru>
2023-03-22 20:29:39 +0300

    Seminar8 Homeworks Final Part!

--- Seminar8/HW60/Program.cs
+++ Seminar8/HW60/Program.cs
@@ -1,3 +1,81 @@
 ﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 //Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 /////////////////////////////////////////////////////////////////////////////////////////////////
+Console.Clear();
+int A = InputValue("Введите величеиу первого измерения массива: ");
+int B= InputValue("Введите величеиу второго измерения массива: ");
+int C = InputValue("Введите величеиу третьего измерения массива: ");
+
+int[,,] array = new int[A, B, C];
+CreateArray(array);
+PrintArray(array);
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//Ввод параметров массива
+static int InputValue(string message)
+{
+    try
+    {
+        Console.Write(message);
+        return (int.Parse(Console.ReadLine() ?? ""));
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return 0;
+    }
+}
+//Создание трёхмерного массива
+void CreateArray(int[,,] arr)
+{
+  int[] temp = new int[arr.GetLength(0) * arr.GetLength(1) * arr.GetLength(2)];
+  int  element;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    element = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          element = temp[i];
+        }
+          element = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      for (int k = 0; k < arr.GetLength(2); k++)
+      {
+        arr[i, j, k] = temp[count];
+        count++;
+      }
+    }
+  }
+}
+
+//Вывод элементов массива на экран
+void PrintArray (int[,,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      
+      for (int k = 0; k < arr.GetLength(2); k++)
+      {
+        Console.Write($"Arr[{i}, {j}, {k}] = {arr[i,j,k]};\t ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
--- Seminar8/HW62/Program.cs
+++ Seminar8/HW62/Program.cs
@@ -1,4 +1,62 @@
 ﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 //Например, на выходе получается вот такой массив:
 /////////////////////////////////////////////////////////////////////////////////////////////////
+Console.Clear();
+int value = InputValue("Введите величеиу первого измерения массива: ");
+int[,] array = new int[value, value];
+
+CompletionArray(array);
+PrintArray(array);
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Ввод параметров массива
+static int InputValue(string message)
+{
+    try
+    {
+        Console.Write(message);
+        return (int.Parse(Console.ReadLine() ?? ""));
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return 0;
+    }
+}
+
+//Заполнение массива
+void  CompletionArray(int[,] arr)
+{
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+
+    while (temp <= array.GetLength(0) * array.GetLength(1))
+    {
+        array[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= array.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > array.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+}
+
+//Вывод массива на экран
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
 
